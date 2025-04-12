5fc239dfaefba4bd74c5bf130bda01735042bc32
wangrui24 <47764869+wangrui24@users.noreply.github.com>
2025-04-12 05:49:23 +0000

    Deploying to gh-pages from @ wangrui24/wangrui24.github.io@94d37ad42eb921024c4b6a824d8a558d8294c9db 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,17 +59,6 @@ ninja.data = [{
             window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
           
         },
-      },{id: "post-a-post-with-custom-blockquotes",
-        
-          title: "a post with custom blockquotes",
-        
-        description: "an example of a blog post with custom blockquotes",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/custom-blockquotes/";
-          
-        },
       },{id: "post-a-post-with-table-of-contents-on-a-sidebar",
         
           title: "a post with table of contents on a sidebar",
