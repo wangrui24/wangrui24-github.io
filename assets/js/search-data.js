778d664dadacbe45c1679d2d8b1cbee36eae9abd
wangrui24 <47764869+wangrui24@users.noreply.github.com>
2025-04-12 05:46:31 +0000

    Deploying to gh-pages from @ wangrui24/wangrui24.github.io@055d179acb588ab636d94212c73005a59faadf15 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,17 +59,6 @@ ninja.data = [{
             window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
           
         },
-      },{id: "post-a-post-with-geojson",
-        
-          title: "a post with geojson",
-        
-        description: "this is what included geojson code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/geojson-map/";
-          
-        },
       },{id: "post-a-post-with-echarts",
         
           title: "a post with echarts",
