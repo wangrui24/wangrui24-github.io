b3389ad212dd11dcf8d090fbbb94e37921dee91f
wangrui24 <47764869+wangrui24@users.noreply.github.com>
2025-04-12 05:57:50 +0000

    Deploying to gh-pages from @ wangrui24/wangrui24.github.io@809ceefed9b048b7664a463020c2c4caa3253f23 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,17 +70,6 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "post-a-post-with-code",
-        
-          title: "a post with code",
-        
-        description: "an example of a blog post with some code",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/code/";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
