c8a06965cde33e67c69086d9cd4fcc809ea00dd4
wangrui24 <47764869+wangrui24@users.noreply.github.com>
2025-04-12 05:49:58 +0000

    Deploying to gh-pages from @ wangrui24/wangrui24.github.io@dcfc02d223c96236370a100b177bf4aa58508a3f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,17 +59,6 @@ ninja.data = [{
             window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
           
         },
-      },{id: "post-a-post-with-table-of-contents-on-a-sidebar",
-        
-          title: "a post with table of contents on a sidebar",
-        
-        description: "an example of a blog post with table of contents on a sidebar",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/sidebar-table-of-contents/";
-          
-        },
       },{id: "post-a-post-with-audios",
         
           title: "a post with audios",
