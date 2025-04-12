83165b3ed281903bdfe05c88b6827c404108a322
wangrui24 <47764869+wangrui24@users.noreply.github.com>
2025-04-12 05:52:54 +0000

    Deploying to gh-pages from @ wangrui24/wangrui24.github.io@0a8e46d66f43077c1a8266466260e3e8c07b7a4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,17 +59,6 @@ ninja.data = [{
             window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
           
         },
-      },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
-        
-          title: "displaying beautiful tables with Bootstrap Tables",
-        
-        description: "an example of how to use Bootstrap Tables",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/tables/";
-          
-        },
       },{id: "post-a-post-with-table-of-contents",
         
           title: "a post with table of contents",
