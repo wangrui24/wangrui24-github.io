a03845c3b96c82a5373378a5f190fe96cd05cf36
wangrui24 <47764869+wangrui24@users.noreply.github.com>
2025-04-12 05:56:28 +0000

    Deploying to gh-pages from @ wangrui24/wangrui24.github.io@654b013f7afd07f88fd588006890ea55e32bcec3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -81,17 +81,6 @@ ninja.data = [{
             window.location.href = "/blog/2015/disqus-comments/";
           
         },
-      },{id: "post-a-post-with-math",
-        
-          title: "a post with math",
-        
-        description: "an example of a blog post with some math",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/math/";
-          
-        },
       },{id: "post-a-post-with-code",
         
           title: "a post with code",
